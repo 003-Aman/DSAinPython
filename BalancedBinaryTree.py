2b2123f1ce6b2bdfc769b2e2c34a58a0a6176ca9
003-Aman <amanshrestha3003@gmail.com>
2024-03-18 17:15:17 -0400

    h

---
+++ BalancedBinaryTree.py
@@ -0,0 +1,3 @@
+#also known as a height-balanced binary tree
+#a binary tree in which the height of the left and right subtree of any node differ by not more than 1
+https://cdn.programiz.com/sites/tutorial2program/files/balanced-binary-tree.png
