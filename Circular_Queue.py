da1995bd3572c0572167e1871412d877b401beac
003-Aman <amanshrestha3003@gmail.com>
2024-03-20 00:11:53 -0400

    Circular Queue revised

--- Circular_Queue.py
+++ Circular_Queue.py
@@ -3,16 +3,20 @@ Applications of Circular Queue
 CPU scheduling
 Memory management
 Traffic Management
+space complexity reduction
+repitions in calendar
+circular manufacturing machines: bottle cap
 '''
 
 class MyCircularQueue:
     def __init__(self,k):
         self.k =k
         self.queue = [None]*k #object banauni bittikai yo value huxa when only parameter k is passed k= no. of items
-        self.head = self.tail = -1 # these are the indexices of the rear and front
+        self.head = self.tail = -1 # these are the indexices of the rear and front at first when nothing is in the queue
 
-    def enqueue(self,data):
+    def enqueue(self,data):#last ma data halna paryo
         if ((self.tail+1)%self.k == self.head):#paxadi full xa ani agadi ko pani 0 index occupied xa vane 
+            #suppose 5 xa length of array ani last ko index will be 4 so , 5%5 = 0 , so aba it will go to the first index
             print("The Circular Queue is full")    
 
         elif (self.head ==-1): #yo vaneko chai aile samma kei halenaraixa vane
@@ -29,30 +33,33 @@ class MyCircularQueue:
             print("The Queue is empty for deque")
 
         elif (self.head == self.tail): #euta matrai element raixa vane queue ma
-            temp= self.queue[self.head]  #head ma jun element xa tei ta niskinxa
-            self.head = -1
+            temp= self.queue[self.head]  #head ma jun element xa tei ta niskinxa, that value is stored in the temp variable
+            self.head = -1#ava tyo euta ni niskiyo so feri duitai -1
             self.tail =-1
-            return temp
+            return temp 
         
         else: 
             temp=self.queue[self.head]
             self.head =(self.head+1)%self.k #elle chai khasma 1 agadi badxa index kinabhane remainder return garxa try it yourself
-            return temp
+            #so if 2 then 3%5 will return 3 not 0 because it returns the remainder vanesi self.head agadi gayo
+            return temp #and the value at 2 was returned
         
     def printCircularQueue(self):
             if(self.head ==-1):
                 print("No element in the circular queue")
         
             elif(self.tail >= self.head ):#euta element matrai vayo vane pani yo condition meet garxa, this is when last ma aako item last mai xa i.e at the right of the array
-                for i in range(self.head, self.tail +1):
+                for i in range(self.head, self.tail +1):#tail +1 kinabhane last to ta include nai hunna so to include that 
                     print(self.queue[i], end = " ")   
                 print()
 
             else:
                 for i in range(self.head,self.k):# this is when last ma aako array thau nabhara agadi pugyo 
+                    #bichha katai bata last samma print garyo
                     print(self.queue[i],end= " ")
                 for i in range(0,self.tail +1):
                     print(self.queue[i],end =" ") 
+                    #tespazxi shuru bata bichha samma
                 print()  
 
 
