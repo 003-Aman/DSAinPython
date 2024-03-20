5ad7ba41a2a9342e3d3aa4ee008b0750dce7bd6e
003-Aman <amanshrestha3003@gmail.com>
2024-03-19 20:37:47 -0400

    adelson velsky and landis

---
+++ AVLtree.py
@@ -0,0 +1,12 @@
+#avl tree (gregory adelson-velsky and landis)
+#avl tree is a self-balancing binary search tree in which each node maintains extra information called a balance factor whose value is eiter -1,0,+1
+
+#Balance Factor
+'''
+balance factor = height of left subtree - height of right subtree
+            or   height of right subtree- height of left subtree
+
+the self-balancing factor of an avl tree is maintained by the balance factor.            
+
+
+'''
