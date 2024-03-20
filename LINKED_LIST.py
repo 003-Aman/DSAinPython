90b375d5fbc999c68c841429395d2bb7f72063d9
003-Aman <amanshrestha3003@gmail.com>
2024-03-20 00:51:13 -0400

    Simple LinkedList revised

--- LINKED_LIST.py
+++ LINKED_LIST.py
@@ -1,5 +1,13 @@
 '''
+linked list is like treasure hunt where each clue includes the information about the next clue
+
 Linked List Applications
+image viewer
+previous and next page in web browser
+music player
+gps navigation
+robotics
+task scheduling, image processing
 Dynamic memory allocation
 Implemented in stack and queue
 In undo functionality of softwares
@@ -7,7 +15,7 @@ Hash tables, Graphs
 '''
 #simple linked list using python
 class Node:# represents a single node in the linked list
-    def __init__(self,item):# object banauni bela you need to pass an arguement
+    def __init__(self,item):# object banauni bela you need to pass an arguement which will be the value that the node holds
         self.item =item #this assigns the value of the node to the item attribute
         self.next = None # initially there is no next node connected to this node
 
@@ -22,9 +30,17 @@ if __name__=="__main__":# this checks if the script is being run directly as the
     linked_list.head = Node(1) #created the first node passing value of item '1' and assigned it to the value of the head of the linkedlist by updating it's attribute variable
     second = Node(2) #two nodes are created using the node class with object name second and third
     third =Node(3) 
+    fourth = Node(7)
+    fifth = Node(8)
+    sixth = Node(9)
 
     linked_list.head.next =second # yo vaneko Node(1) which is linked_list.head is connected to the second node y updating or setting next attribute to second
     second.next = third  #third and second connected tesari nai
+    third.next = fourth
+    fourth.next = fifth
+    fifth.next = sixth
+
+
 
     while linked_list.head != None: # this line will run as long as the head of the linkedlist is not None which means the linkedlist is not empty
         print(linked_list.head.item, end ="")#This line prints the value of the current node (linked_list.head.item) without moving to the next node in the list
